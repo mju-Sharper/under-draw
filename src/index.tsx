7b649527ec738cc46bd7599a9577498063e38327
harksu <harksulim@naver.com>
2023-03-13 16:15:05 +0900

    chore: root setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import './index.css';
 
@@ -11,9 +11,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
