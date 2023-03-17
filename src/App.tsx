861a609e73eb8836912f8958b10ec688c6d0898a
harksu <harksulim@naver.com>
2023-03-17 11:23:27 +0900

    chore: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import MainPage from './pages/MainPage';
 import { Theme } from './styles/Theme';
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={Theme} />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,3 @@
+const MainPage = () => <div>저는 메인페이지에용</div>;
+
+export default MainPage;
