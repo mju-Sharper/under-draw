b7f2990ee36c56b1cd2c6aaea7332cd7a02cb7d1
harksu <harksulim@naver.com>
2023-03-17 11:02:55 +0900

    setting: theme provider setting: setting.json

---
+++ src/styles/Theme.ts
@@ -0,0 +1,13 @@
+import type { DefaultTheme } from 'styled-components';
+
+export const Theme: DefaultTheme = {
+  colors: {
+    NAVY: '#222832',
+    WTHIE: '#FFFFFF',
+    BLACK: '#000000',
+    GRAY: '1B1B1B',
+    PURPLE: '6462B9',
+    PINK: 'E8AAAA',
+    LIGTH_GRAY: '8D8D8D',
+  },
+};
