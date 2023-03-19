bb862833deafc785266aac7f7936ea43f0741af9
harksu <harksulim@naver.com>
2023-03-19 23:39:32 +0900

    chore: eslint prettier rule add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import './App.css';
 
 import MainPage from './pages/MainPage';
+import Registration from './pages/Registration';
 import GlobalStyle from './styles/GlobalStyle';
 import Theme from './styles/Theme';
 
@@ -13,6 +14,7 @@ function App() {
         <GlobalStyle />
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/Registration" element={<Registration />} />
         </Routes>
       </ThemeProvider>
     </div>
---
+++ src/pages/Registration/index.tsx
@@ -0,0 +1,3 @@
+const Registration = () => <div>Registration</div>;
+
+export default Registration;
