65f37a90640acff58b22e33525ec701cdee9eddc
JJongsKim <totnwjd530@gmail.com>
2023-03-21 13:13:03 +0900

    :memo: setting: globalstyle 관련 수정
    - 페이지 중앙 정렬

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,6 +7,9 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
     height: 100%;
     margin: 0 auto;
