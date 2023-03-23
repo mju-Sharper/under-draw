1f5e7b9b052799fe8a5fef9ff3f7e775d843fb02
JJongsKim <totnwjd530@gmail.com>
2023-03-23 21:21:22 +0900

    :lipstick: style: 회원가입, 로그인 박스창 스타일링

--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -1,3 +1,18 @@
-const SignIn = () => <>로그인 페이지</>;
+import styled from 'styled-components';
+
+import AuthFormBox from '../../components/Auth/AuthModalBox';
+
+const SignIn = () => (
+  <SignInPageWrap>
+    <AuthFormBox authTitle={`LOGIN`}>로그인 페이지</AuthFormBox>
+  </SignInPageWrap>
+);
+
+const SignInPageWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 export default SignIn;
--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -1,3 +1,20 @@
-const SignUp = () => <>회원가입 페이지</>;
+import styled from 'styled-components';
+
+import AuthFormBox from '../../components/Auth/AuthModalBox';
+
+const SignUp = () => (
+  <SignUpPageWrap>
+    <AuthFormBox authTitle={`SIGN-UP`} height={700}>
+      회원가입 페이지
+    </AuthFormBox>
+  </SignUpPageWrap>
+);
+
+const SignUpPageWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 export default SignUp;
