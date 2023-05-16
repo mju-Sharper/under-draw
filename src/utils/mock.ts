b0ac0b387f661e253a472cdc25a5452eca8040c6
harksu <harksulim@naver.com>
2023-05-17 01:53:50 +0900

    chore: add category jewel

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -4,6 +4,7 @@ import ElectronicIcon from '../assets/ElectronicIcon.svg';
 import FashionIcon from '../assets/FashionIcon.svg';
 import FurnitureIcon from '../assets/FurnitureIcon.svg';
 import HouseIcon from '../assets/HouseIcon.svg';
+import JewelleryIcon from '../assets/JewelleryIcon.svg';
 import OfficeIcon from '../assets/OfficeIcon.svg';
 import OtherIcon from '../assets/OtherIcon.svg';
 import OutdoorIcon from '../assets/OutdoorIcon.svg';
@@ -12,7 +13,7 @@ export const categoryItemList = [
   { id: 'electronic', name: 'HOME_APPLICES', icon: ElectronicIcon },
   { id: 'outdoor', name: 'SPORT', icon: OutdoorIcon },
   { id: 'furniture', name: 'FURNITURE', icon: FurnitureIcon },
-  // { id: 'jewellery', name: '귀금속', icon: JewelleryIcon },
+  { id: 'jewel', name: 'JEWEL', icon: JewelleryIcon },
   { id: 'car', name: 'CAR', icon: CarIcon },
   { id: 'fashion', name: 'FASHION', icon: FashionIcon },
   { id: 'office', name: 'OFFICE', icon: OfficeIcon },
